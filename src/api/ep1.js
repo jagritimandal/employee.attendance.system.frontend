2f6f279d70bf9246526d76109d6e8688b413cb09
jagriti mandal <jagriti.mandal.csds20@teamfuture.in>
2025-08-26 14:04:09 +0530

    epi url changed

--- src/api/ep1.js
+++ src/api/ep1.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const ep1 = axios.create({
-  baseURL: "http://localhost:5000", // no spaces
+  baseURL: "https://employee-attendance-system-1w50.onrender.com",
+  //baseURL: "http://localhost:5000", // no spaces
   headers: {
     "Content-Type": "application/json",
   },
