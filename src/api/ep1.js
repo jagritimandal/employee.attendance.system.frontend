2e768ec12303a52a6eef5793c7a217b7095c2448
jagriti mandal <jagriti.mandal.csds20@teamfuture.in>
2025-08-29 09:17:34 +0530

    ip manager changed and added pagination installed

--- src/api/ep1.js
+++ src/api/ep1.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const ep1 = axios.create({
-  //baseURL:"https://ctserver1.azurewebsites.net/",
-  baseURL: "https://employee-attendance-system-1w50.onrender.com",
+  baseURL:"https://ctserver1.azurewebsites.net/",
+  //baseURL: "https://employee-attendance-system-1w50.onrender.com",
   //baseURL: "http://localhost:5000", // no spaces
   headers: {
     "Content-Type": "application/json",
